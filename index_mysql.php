10977ef9c174d51d36f9b55721fd3b0b1b2dfda3
unknown <227006055@student.unsil.ac.id>
2023-09-23 09:11:31 +0700

    first commit

---
+++ index_mysql.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1" cellpadding=10 cellspacing=0>
+        <tr>
+            <td>No.</td>
+            <td>Nama</td>
+            <td>NPM</td>
+            <td>Jurusan</td>
+            <td>Fakultas</td>
+            <td colspan=2>Aksi</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Yuldan Nur Addinsyah</td>
+            <td>227006091</td>
+            <td>Informatika</td>
+            <td>Teknik</td>
+            <td>hapus</td>
+            <td>edit</td>
+        </tr>
+    </table>
+</body>
+</html>
